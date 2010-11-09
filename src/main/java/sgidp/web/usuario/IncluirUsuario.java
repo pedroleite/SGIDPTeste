13adc8605454170e12b4fbedc5b1411932dc57ec
Pedro <pedroleite.s@gmail.com>
2010-11-09 00:15:13 -0200

    Atualizando Projeto

--- src/main/java/sgidp/web/usuario/IncluirUsuario.java
+++ src/main/java/sgidp/web/usuario/IncluirUsuario.java
@@ -4,44 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.wicket.PageParameters;
-import org.apache.wicket.extensions.markup.html.form.palette.Palette;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.IChoiceRenderer;
-import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.model.util.CollectionModel;
 import org.apache.wicket.model.util.ListModel;
 
-import br.com.pw.sgidp.negocio.PermissaoBO;
-import br.com.pw.sgidp.negocio.UsuarioBO;
-import br.com.pw.sgidp.negocio.entidade.Permissao;
-import br.com.pw.sgidp.negocio.entidade.Usuario;
-
 import sgidp.web.template.BasePage;
+import br.com.pw.sgidp.negocio.PermissaoBO;
+import br.com.pw.sgidp.negocio.entidade.Permissao;
 
 public class IncluirUsuario extends BasePage {
 
-
-
-	
 	public IncluirUsuario(final PageParameters parameters) {
 		super(parameters, "Usuário", "Incluir usuário");
-		Form f=new Form("form");
-	    add(f);
-	    
-	    List<Permissao> permissoes = (List<Permissao>) new PermissaoBO().getListaTodasPermissoes();
-	    
-	    IChoiceRenderer renderer = new ChoiceRenderer("descricao", "codigo");
-	    
-	    final Palette palette = new Palette("palette", new ListModel<Permissao>(new ArrayList<Permissao>()), new CollectionModel<Permissao>(
-	    		permissoes), renderer, 10, true);
-	    	    
-	    f.add(palette);
-	    
-	    
+		Form f = new Form("form");
+		add(f);
+
+		List<Permissao> permissoes = (List<Permissao>) new PermissaoBO()
+				.getListaTodasPermissoes();
+
+		IChoiceRenderer renderer = new ChoiceRenderer("descricao", "codigo");
+
+		final Palette palette = new Palette("palette",
+				new ListModel<Permissao>(new ArrayList<Permissao>()),
+				new CollectionModel<Permissao>(permissoes), renderer, 10, true);
+
+		f.add(palette);
 
 	}
 
-	
 }
